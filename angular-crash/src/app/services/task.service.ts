f0a8e7ce68cbb8b486d39019657d4fead672f65c
jamestheengineer <jamestheengineer@gmail.com>
2023-02-10 20:23:02 -0500

    Hook up backend to frontend with http client

--- angular-crash/src/app/services/task.service.ts
+++ angular-crash/src/app/services/task.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock-tasks';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
