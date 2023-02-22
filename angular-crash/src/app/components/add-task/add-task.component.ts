c72cb7189efc5a942f6fa942d01c23354055c944
jamestheengineer <jamestheengineer@gmail.com>
2023-02-21 19:59:21 -0500

    Show hide task input based on event from button

--- angular-crash/src/app/components/add-task/add-task.component.ts
+++ angular-crash/src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/Task';
+
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
@@ -10,6 +13,14 @@ export class AddTaskComponent {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   onSubmit() {
     if (!this.text) {
